233571ecc00d8c7923e1011eb8028c2b89d71952
danelian <141317269+danelian@users.noreply.github.com>
2023-12-07 15:37:39 +0300

    header - desktop

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
 
+  /*=== Search (Header) ==================================*/
+  let searchForm = document.querySelector("#searchForm");
+  let searchInput = document.querySelector("#searchForm .search-input");
+  let searchButton = document.querySelector("#searchButton");
+  searchButton.addEventListener("click", (e) => {
+    if (!searchForm.classList.contains('active')) {
+      e.preventDefault();
+    }
+    searchForm.classList.toggle("active");
+    searchInput.focus();
+  });
+  window.onscroll = () => {
+    searchForm.classList.remove("active");
+  }
+  document.addEventListener("click", (event) => {
+    if (!searchForm.contains(event.target)) {
+      searchForm.classList.remove("active");
+    }
+  });
+	// document.addEventListener("click", (event) => {
+	// 	if (!event.target.matches("#searchForm, #searchButton, #searchButton i, #searchInput")) {
+	// 		searchForm.classList.remove("active");
+	// 	}
+	// });
+
 });
